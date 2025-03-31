c5482e7a4a28712c2a38fff83296f175a5edc7d5
cyanjz <yozdaz@gmail.com>
2025-03-31 17:38:18 +0900

    .

---
+++ articlepjt/articles/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from .forms import ArticleForm
+from .models import Article
+
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles' : articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+# 생성
+def create(request):
+    if request.method =='POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'articles/create.html', context)
+
+
+# 세부 내용
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+# 업데이트
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'form' : form,
+        'article' : article,
+    }
+    return render(request, 'articles/update.html', context)
